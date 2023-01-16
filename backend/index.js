96cd6c887c96d15b54c7be1bc1002905ffaf2f9b
Ziva1011 <catarina.vi.pires@gmail.com>
2023-01-16 16:08:51 +0100

    jwt auth

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const repo = require('./repository')
+const jwt = require("jsonwebtoken");
+
 var cors = require('cors')
+
 const app = express()
 
-let _items = require('./users.json');
+
+let _users = require('./users.json');
+let _items = require('./items.json');
+const cookieParser = require('cookie-parser');
 
 
 function getUserPurchases(userId) {
-    var JSONObject = _items
+    var JSONObject = _users
 	for (i=0; i < JSONObject.length; i++) {
 
    	 	if (JSONObject[i].id == userId)
@@ -18,11 +24,15 @@ function getUserPurchases(userId) {
 }
 
 function hasValue(email,password) {
-    var JSONObject = _items
+    var JSONObject = _users
 	for (i=0; i < JSONObject.length; i++) {
 
-   	 	if (JSONObject[i].password == password && JSONObject[i].email == email)
+   	 	if (JSONObject[i].password == password && JSONObject[i].email == email){
+			const token = jwt.sign({ username }, 
+			process.env.JWT_SECRET_KEY,{
+			expiresIn: 86400});
         	return JSONObject[i];
+		}
 	}
 	return null;
 }
@@ -32,6 +42,7 @@ function hasValue(email,password) {
 
 
 app.use(cors())
+app.use(cookieParser())
 
 const port = process.env.PORT_BACKEND || 3000
 
@@ -47,22 +58,44 @@ app.get('/signup', (req, res) => {}),
 // logic and add data to the database
 app.post('/login', async (req, res) => {
 	const {email, password} = req.body
-	console.log(req.body)
-	let result=hasValue(email, password)
-	//const addedRecord = await
-	// 	repo.createNewRecord({email, password})
-
-	// console.log(`Added Record :
-	// 	${JSON.stringify(addedRecord, null, 4)}`)
-	res.send(result)
+	var JSONObject = _users
+	for (i=0; i < JSONObject.length; i++) {
+		if (JSONObject[i].password == password && JSONObject[i].email == email){
+				let id=JSONObject[i].id
+				const token = jwt.sign({id},
+				"poihuz7t6g",{
+				expiresIn: 86400});
+				let user = JSONObject[i];
+				return res
+					//.cookie("access_token",token, {httpOnly: true, secure: false})
+					.json({ user, msg: "Login Success" , token: token}); 
+			}
+	}
 })
 
 app.post('/purchases', (req, res) => {
-	const {userId} = req.body
+	const {token} = req.body;
+	var userId = 0;
+	if (!token){
+		throw new Error("Error");
+	}
+	try{
+		var data = jwt.verify(token, "poihuz7t6g")
+		userId=  data.id
+	}catch{
+		console.log("Token could not be verified")
+		return null
+	}
+	//const {userId} = req.body
 	let purchases = getUserPurchases(userId)
 	res.send(purchases)
-
 }),
+
+// app.get('/logout', (req, res) => {
+// 	return res
+// 		//.clearCookie("access_token")
+// 		.status(200)
+// }),
 
 
 // Server setup
